24d1c42d6d062341748c30fe402049907c11a08e
Dru Serkes <dru@zephyrx.com>
2021-06-04 02:00:10 -0500

    add 128 icon, update manifest, regex

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const replacementPhrase = "45";
+const regex = new RegExp(/trump|donald j. trump|donald trump|donald\s*j.*\s*trump|donald \w* trump/, 'gi');
 
 /**
  * 
@@ -8,7 +9,7 @@ const purge45 = (element) => {
   if (element.hasChildNodes()) {
     element.childNodes.forEach(purge45);
   } else if (element.nodeType === Text.TEXT_NODE) {
-    element.textContent = element.textContent.replace(/trump/gi, replacementPhrase);
+    element.textContent = element.textContent.replace(regex, replacementPhrase);
   };
 };
 
